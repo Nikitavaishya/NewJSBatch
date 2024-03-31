4130fe208ea46117c115901de049bdaa1f71db0c
Nikitavaishya <90676096+Nikitavaishya@users.noreply.github.com>
2024-03-31 15:08:12 +0530

    First committed

---
+++ first.js
@@ -0,0 +1,24 @@
+// console.log('hi Kishna') ; 
+// window.alert("hello") ;
+// log- print 
+// output statements 
+// 1. console.log 
+// 2.window.alert()  
+// 3.document.write() 
+// 4.innerHTML()  
+// Input Statements 
+// 1. window.prompt() ; 
+// 2.window.confirm() ; 
+// window.prompt("name") ;
+// variables 
+var firstName = "Kishna" ;  
+console.log(firstName) ;
+// var,Let,const
+// rules 
+// 1.case sensitive  , a & A  is different; 
+// 2. letters, digit,underscore_ ,$ ,  space not allowed.   
+// 3.only a letter, underScore(_) , $ should be 1st character .
+// 4.Reserved word can not be  variable names.
+var firstName123 = '' ;
+// var 123firstName = "" ,-- not allowed 
+
